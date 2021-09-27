31de18c78cc31f3fb118cf0ac9fecb6c01dc3f87
Anthony VERCHERE <76996157+Anthony69600@users.noreply.github.com>
2021-09-27 22:11:08 +0200

    second_commit

--- index.php
+++ index.php
@@ -8,17 +8,16 @@ echo "Arme d'un opposant : $opponentWeapon" . '</br>';
 
 switch ($opponentWeapon) {
     case ($opponentWeapon == $weapons[0]):
-        $indyWeapon = 'gun';
+        $indyWeapon = $weapons[2];
         break;
     case ($opponentWeapon == $weapons[1]):
-        $indyWeapon = 'first';
-        break;
-    case ($opponentWeapon == $weapons[2]):
-        $indyWeapon = 'whip';
+        $indyWeapon = $weapons[0];
         break; 
+    case ($opponentWeapon == $weapons[2]):
+        $indyWeapon = $weapons[1];
+        break;
     default:
         break; 
 }
 
 echo "Arme d'Indy est : $indyWeapon";
-
